595b2b5dd36ddff282e5e190dda64d94c1d174ac
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-03-15 11:17:56 +0530

    Sort By category and price

--- src/Components/Card/ProductCard.tsx
+++ src/Components/Card/ProductCard.tsx
@@ -3,7 +3,9 @@ const ProductCard = (props: any) => {
   return (
     <div className="solo">
       <img src={props.image} alt={`image of the book ${props.title}`} />
-      <h4>{props.title}</h4>
+      <div className="title">
+        <h4>{props.title}</h4>
+      </div>
       <h4>₹ {props.price}</h4>
     </div>
   );
--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -3,6 +3,8 @@ import { getAllProducts } from "../../../api/firebase";
 import ProductCard from "../Card/ProductCard";
 import "./Product.css";
 const Product = () => {
+  const [category, setCategory] = React.useState<string | null>("all");
+  const [order, setOrder] = React.useState<string | null>(null);
   const [data, setData] = React.useState<
     {
       category: string;
@@ -33,18 +35,70 @@ const Product = () => {
   }, []);
 
   function productElement() {
-    return data.map((curr) => (
-      <ProductCard
-        key={curr.id}
-        title={curr.title}
-        image={curr.image}
-        price={curr.price}
-      />
-    ));
+    return data
+      .sort(
+        (
+          a: {
+            category: string;
+            description: string;
+            id: string;
+            image: string;
+            pages: number;
+            price: number;
+            title: string;
+          },
+          b: {
+            category: string;
+            description: string;
+            id: string;
+            image: string;
+            pages: number;
+            price: number;
+            title: string;
+          }
+        ) => {
+          if (order === "descending") return b.price - a.price;
+          else if (order === "ascending") return a.price - b.price;
+          else return Math.random() - 0.5;
+        }
+      )
+      .map((curr) => {
+        if (category !== "all" && curr.category !== category) return;
+        return (
+          <ProductCard
+            key={curr.id}
+            title={curr.title}
+            image={curr.image}
+            price={curr.price}
+          />
+        );
+      });
   }
+
+  function filterByCategory(e: React.SyntheticEvent) {
+    let target = e.target as HTMLButtonElement;
+    setCategory(target.textContent);
+  }
+  function sortByPrice(e: React.SyntheticEvent) {
+    let target = e.target as HTMLButtonElement;
+    setOrder(target.textContent);
+  }
+
   return (
     <>
       <h1 className="product-header">Products</h1>
+      <div className="category-filter">
+        <button onClick={filterByCategory}>non-fiction</button>
+        <button onClick={filterByCategory}>biography</button>
+        <button onClick={filterByCategory}>fantasy</button>
+        <button onClick={filterByCategory}>young-adult</button>
+        <button onClick={filterByCategory}>all</button>
+      </div>
+      <div className="category-filter">
+        <button onClick={sortByPrice}>ascending</button>
+        <button onClick={sortByPrice}>descending</button>
+        <button onClick={sortByPrice}>random</button>
+      </div>
       <div className="thumb-container">{productElement()}</div>
     </>
   );
